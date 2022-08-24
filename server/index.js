259cadef274d98f841de3224e7af2bb103156b03
sarb908 <sarbjots908@gmail.com>
2022-08-24 23:07:39 +0530

    day-2

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const connection = require("./config");
 const authRouter = require("./controllers/authController");
-
+const passport = require("./googleAuth");
 const app = express();
 app.use(express.json());
 app.get("/", (req, res) => {
@@ -10,7 +10,29 @@ app.get("/", (req, res) => {
 });
 
 app.use("/", authRouter);
-app.listen(process.env.PORT || 8080, async () => {
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+    session: false,
+  }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    //console.log(req.user);
+    res.redirect("/dashboard");
+  }
+);
+app.get("/dashboard", (req, res) => {
+  res.send("COOL");
+});
+
+app.listen(process.env.PORT || 7000, async () => {
   try {
     await connection;
     console.log("database connected");
